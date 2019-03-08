c011497170c1e0952df2bce60f1a37a45b0a50d7
donequick <donequick@outlook.com>
2019-03-08 23:51:40 +0800

    correct structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quickdone",
-    version="0.0.5",
+    version="0.0.8",
     author="quickdone",
     author_email="donequick@outlook.com",
     description="A handy toolkit",
